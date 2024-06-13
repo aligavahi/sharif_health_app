af61e9d30416fdc7172658e036fd524c3a8a2f57
Ali Gavahi <ali.gavahi@tapsi.cab>
2024-06-13 23:03:28 +0330

    fix first page

--- lib/ui/login/first_page.dart
+++ lib/ui/login/first_page.dart
@@ -8,52 +8,65 @@ class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("assets/1.png"),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: Column(
-        children: [
-          const Spacer(
-            flex: 8,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/first1.png"),
+            fit: BoxFit.cover,
           ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: 60,
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(0.0),
+        ),
+        child: Row(children: [
+          Spacer(),
+          Container(
+            constraints: const BoxConstraints(maxWidth: 400),
+            child: Column(
+              children: [
+                const Spacer(
+                  flex: 2,
                 ),
-                padding: EdgeInsets.zero,
-                elevation: 4,
-                backgroundColor: Colors.transparent,
-              ),
-              onPressed: () {
-                BlocProvider.of<LoginCubit>(context).goPhoneNumber();
-              },
-              child: Ink.image(
-                image: const AssetImage('assets/2.png'),
-                fit: BoxFit.cover,
-                child: const SizedBox(
-                  width: double.infinity,
-                  height: double.infinity,
-                  child: Center(
-                    child: Text(
-                      'ورود/ثبت نام',
+                SizedBox(
+                    height: MediaQuery.of(context).size.height * .3,
+                    child: Image.asset("assets/first2.png")),
+                const Spacer(
+                  flex: 8,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * .6,
+                  height: 60,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(0.0),
+                      ),
+                      padding: EdgeInsets.zero,
+                      elevation: 4,
+                      backgroundColor: Colors.transparent,
+                    ),
+                    onPressed: () {
+                      BlocProvider.of<LoginCubit>(context).goPhoneNumber();
+                    },
+                    child: Ink.image(
+                      image: const AssetImage('assets/2.png'),
+                      fit: BoxFit.cover,
+                      child: const SizedBox(
+                        width: double.infinity,
+                        height: double.infinity,
+                        child: Center(
+                          child: Text(
+                            'ورود/ثبت نام',
+                          ),
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
+                const Spacer(
+                  flex: 1,
+                ),
+              ],
             ),
           ),
-          const Spacer(
-            flex: 1,
-          ),
-        ],
-      ),
-    );
+          Spacer()
+        ]));
   }
 }
