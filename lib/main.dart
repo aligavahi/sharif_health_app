d11ce1cdf90439ae92f6c44f7ae8dbd34b209e82
Ali Gavahi <ali.gavahi@tapsi.cab>
2024-06-13 20:39:12 +0330

    add font

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          fontFamily: 'Kalameh',
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
           useMaterial3: true,
         ),
         routes: {
