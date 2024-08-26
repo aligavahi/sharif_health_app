d7224fb467a2ca1f97e77ad95dae281a373cf035
Ali Gavahi <ali.gavahi@tapsi.cab>
2024-08-27 00:06:05 +0330

    finish general section

--- lib/ui/homepage/my_tests/general_analysis.dart
+++ lib/ui/homepage/my_tests/general_analysis.dart
@@ -1,30 +1,30 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:sharif_health_app/model/homepage/my_tests/my_tests_cubit.dart';
-import 'package:sharif_health_app/utils/gradient_text.dart';
+import 'package:sharif_health_app/utils/app_colors.dart';
 
 class GeneralAnalysis extends StatelessWidget {
-  GeneralAnalysis({super.key, required this.data});
+  const GeneralAnalysis({super.key, required this.data});
 
   final DeviceData data;
-  final gradientMainColor = () =>
-      LinearGradient(colors: [Colors.green.shade800, Colors.blue.shade800]);
 
   @override
   Widget build(BuildContext context) {
     List<Map<String, dynamic>> typ1Analysis = [
       {
-        'name': 'قد',
+        'name': 'قد شما',
         'unit': 'سانتی متر',
-        'icon': 'general1_icon1.png',
+        'icon': 'general_height.png',
         'value': data.height,
-        'percentage': ''
       },
       {
-        'name': 'وزن',
+        'name': 'وزن شما',
         'unit': 'کیلوگرم',
-        'icon': 'general1_icon2.png',
+        'icon': 'general_weight.png',
         'value': data.weight,
-        'percentage': ''
       },
     ];
 
@@ -32,150 +32,147 @@ class GeneralAnalysis extends StatelessWidget {
       {
         'name': 'سن متابولیک',
         'unit': 'سال',
-        'icon': 'general2_icon1.png',
-        'image': 'general2_image1.png',
+        'icon': 'general_icon1.png',
+        'image': 'general_back1.png',
         'value': data.metabolic_age,
         'percentage': ''
       },
       {
         'name': 'شاخص توده بدنی',
         'unit': '',
-        'icon': 'general2_icon2.png',
-        'image': 'general2_image2.png',
+        'icon': 'general_icon2.png',
+        'image': 'general_back2.png',
         'value': data.bmi,
-        'percentage': ''
-      },
-      {
-        'name': 'متابولیسم پایه',
-        'unit': 'کیلوکالری',
-        'icon': 'general2_icon3.png',
-        'image': 'general2_image3.png',
-        'value': data.bmr,
         'percentage': ''
       },
       {
         'name': 'میزان آب بدن',
         'unit': 'لیتر',
-        'icon': 'general2_icon4.png',
-        'image': 'general2_image4.png',
+        'icon': 'general_icon3.png',
+        'image': 'general_back3.png',
         'value': data.total_water_weight,
         'percentage': data.total_water_percentage
+      },
+      {
+        'name': 'متابولیسم پایه',
+        'unit': 'کیلوکالری',
+        'icon': 'general_icon4.png',
+        'image': 'general_back4.png',
+        'value': data.bmr,
+        'percentage': ''
       },
     ];
     final width = (MediaQuery.of(context).size.width - 30) / 2;
     List<Widget> final_list = [];
     List<Widget> typ1_items = List<Widget>.generate(typ1Analysis.length,
-            (index) => get_general_typ1_item(context, typ1Analysis[index]));
+        (index) => get_general_typ1_item(context, typ1Analysis[index]));
     List<Widget> typ2_items = List<Widget>.generate(typ2Analysis.length,
-            (index) => get_general_typ2_item(context, typ2Analysis[index]));
+        (index) => get_general_typ2_item(context, typ2Analysis[index]));
     final_list.add(SizedBox(
-      height: (width + 5) * .5 * 1,
+      height: (width + 5) * .33 * 1 + 20,
       child: GridView.count(
-        childAspectRatio: 2,
+        childAspectRatio: 3,
         controller: new ScrollController(keepScrollOffset: false),
         shrinkWrap: true,
         scrollDirection: Axis.vertical,
+        padding: EdgeInsets.all(20),
+        crossAxisSpacing: 8,
+        mainAxisSpacing: 8,
         crossAxisCount: 2,
         children: typ1_items,
       ),
     ));
     final_list.add(SizedBox(
-      height: (width + 2) * 1 * 2,
+      height: (width + 2) * 1 * 2 + 2 * 20,
       child: GridView.count(
+        padding: EdgeInsets.all(20),
         childAspectRatio: 1,
         controller: new ScrollController(keepScrollOffset: false),
         shrinkWrap: true,
         scrollDirection: Axis.vertical,
         crossAxisCount: 2,
+        crossAxisSpacing: 8,
+        mainAxisSpacing: 8,
         children: typ2_items,
       ),
     ));
-    return const Placeholder();
+    return ListView(
+      children: final_list,
+    );
   }
-
 
   get_general_typ1_item(context, Map<String, dynamic> item) {
     return Card(
-      margin: EdgeInsets.all(10),
-      child: Container(
-        decoration: BoxDecoration(
-            gradient: gradientMainColor(),
-            borderRadius: BorderRadius.all(Radius.circular(20))),
+      color: AppColors.white,
+      child: Center(
         child: ListTile(
-          contentPadding: EdgeInsets.all(10),
           subtitle: Text(
             "${item['value']} ${item['unit']}",
             textDirection: TextDirection.rtl,
-            style: const TextStyle(color: Colors.white),
-            textAlign: TextAlign.center,
+            textAlign: TextAlign.right,
           ),
           title: Text(
             item['name'],
-            style: const TextStyle(
-              color: Colors.white,
-            ),
             textAlign: TextAlign.right,
           ),
-          leading: Image.asset("assets/mytests/${item['icon']}"),
-          isThreeLine: true,
+          leading: Container(
+            decoration: const BoxDecoration(
+              color: AppColors.white,
+              shape: BoxShape.circle,
+              boxShadow: [
+                BoxShadow(
+                    blurRadius: 5, color: AppColors.shadow, spreadRadius: 3)
+              ],
+            ),
+            child: CircleAvatar(
+                backgroundColor: AppColors.background,
+                radius: 30,
+                child: Padding(
+                    padding: EdgeInsets.all(5),
+                    child: Image.asset("assets/mytests/${item['icon']}"))),
+          ),
         ),
       ),
     );
   }
 
-  get_general_typ2_item(context, Map<String, dynamic> item) {
-    return Card(
-      color: Colors.white,
-      shadowColor: Colors.black,
-      margin: EdgeInsets.all(10),
-      child: Container(
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.all(Radius.circular(20))),
-        child: Column(
-          children: [
-            ListTile(
-              iconColor: Colors.green,
-              contentPadding: EdgeInsets.all(10),
-              title: GradientText(
-                text: Text(
+  Widget get_general_typ2_item(context, Map<String, dynamic> item) {
+    return AspectRatio(
+        aspectRatio: 1,
+        child: Container(
+          padding: EdgeInsets.zero,
+          margin: EdgeInsets.zero,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(
+                    "assets/mytests/${item['image']}",
+                  ))),
+          child: InkWell(
+            child: ListTile(
+                subtitle: Text(
+                  "${item['value']} ${item['unit']}",
+                  textDirection: TextDirection.rtl,
+                  textAlign: TextAlign.right,
+                ),
+                title: Text(
                   item['name'],
                   textAlign: TextAlign.right,
-                  style: TextStyle(fontWeight: FontWeight.bold),
                 ),
-                gradient: gradientMainColor(),
-              ),
-              trailing: Image.asset("assets/mytests/${item['icon']}"),
-            ),
-            Expanded(
-              child: Center(
-                child: ListTile(
-                  leading: GradientText(
-                    text: Text(
-                      "${item['value']} ${item['unit']}",
-                      textDirection: TextDirection.rtl,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontWeight: FontWeight.bold),
+                trailing: Container(
+                    width: 50,
+                    height: 50,
+                    margin: const EdgeInsets.only(right: 10),
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      color: Color.fromRGBO(250, 250, 250, .3),
+                      shape: BoxShape.rectangle,
                     ),
-                    gradient: gradientMainColor(),
-                  ),
-                  title: Image.asset("assets/mytests/${item['image']}"),
-                  trailing: GradientText(
-                    text: Text(
-                      check_null(item),
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    gradient: gradientMainColor(),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+                    child: Image.asset("assets/mytests/${item['icon']}"))),
+          ),
+        ));
   }
+
   String check_null(Map<String, dynamic> item) {
     return item['percentage'] == '' ? "" : "${item['percentage']} %";
   }
--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -6,7 +6,10 @@ class AppColors {
   static const Color green = Color.fromRGBO(0x36, 0xC9, 0xC2, 1);
   static const Color background = Color.fromRGBO(0xF6, 0xF7, 0xFC, 1);
   static const Color black = Color.fromRGBO(0x55, 0x55, 0x55, 1);
+  static const Color shadow = Color.fromRGBO(0x66, 0x66, 0x66, 1);
   static const Color white = Colors.white;
 
   static const Color orange = Color.fromRGBO(0xFD, 0x82, 0x0B, 1);
+
+
 }
