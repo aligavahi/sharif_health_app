7b6c0a0ff5691508f5f64b4627396afbb818d267
Ali Gavahi <ali.gavahi@tapsi.cab>
2024-08-27 00:40:04 +0330

    try to adding history page

---
+++ lib/ui/homepage/my_tests/history_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+class HistoryPage extends StatelessWidget {
+  final String name;
+  final List<Map> data;
+
+  const HistoryPage({super.key, required this.name, required this.data});
+
+  @override
+  Widget build(BuildContext context) {
+    final Map<String, String> ranges = {"": ""};
+    final Map<String, String> details = {"": ""};
+    final Map<String, String> title = {"": ""};
+
+    Widget chartWidget = SfCartesianChart(
+        primaryXAxis: const CategoryAxis(),
+        series: <CartesianSeries>[
+          LineSeries<Map, DateTime>(
+              dataSource: data,
+              xValueMapper: (Map item, _) => item['x'],
+              yValueMapper: (Map item, _) => item['y'])
+        ]);
+    Widget rangeWidget = Container(
+      height: 100,
+      child: Column(
+        children: [
+          Expanded(
+              child: Container(
+            child: Text("بازه و محدوده شما"),
+          )),
+          Expanded(
+              child: Container(
+            child: Text(ranges[name] ?? ""),
+          )),
+        ],
+      ),
+    );
+    Widget stackWidget = Stack(
+      children: [
+        chartWidget,
+        Positioned(
+          child: rangeWidget,
+          bottom: 40,
+        )
+      ],
+    );
+    Widget titleText = Text(title[name] ?? "");
+    Widget detailText = Text(details[name] ?? "");
+    return Column(
+      children: [stackWidget, titleText, detailText],
+    );
+  }
+}
