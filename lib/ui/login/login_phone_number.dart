84db3774c72405331d336c58a8894273cd3c1405
MohammadJavad Fallahpour <mojefline@gmail.com>
2024-09-18 13:35:00 +0330

    login phone number new figma done

--- lib/ui/login/login_phone_number.dart
+++ lib/ui/login/login_phone_number.dart
@@ -3,12 +3,13 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sharif_health_app/model/login/login_cubit.dart';
+import 'package:sharif_health_app/utils/app_colors.dart';
 
 class LoginPhoneNumber extends StatelessWidget {
   const LoginPhoneNumber({super.key});
 
   final String title = "ورود | ثبت نام";
-  final String detail = "به منظور ورود شماره همراه خود را وارد کنید";
+  final String detail = "شماره همراه خود را وارد کنید";
   final String buttonText = "ورود";
 
   @override
@@ -16,58 +17,81 @@ class LoginPhoneNumber extends StatelessWidget {
     return Container(
       decoration: const BoxDecoration(
         image: DecorationImage(
-          image: AssetImage("assets/4.png"),
-          fit: BoxFit.cover,
+          fit: BoxFit.fill,
+          image: AssetImage("assets/page2/login_number_input.png"),
         ),
       ),
       child: getForm(context),
     );
   }
 
-  Widget getForm(context) {
+  Widget getForm(BuildContext context) {
     double max_item_height = MediaQuery.of(context).size.height * .35 / 4;
     return Container(
         margin: EdgeInsets.fromLTRB(
-          40,
-          MediaQuery.of(context).size.height * .15,
-          40,
-          MediaQuery.of(context).size.height * .20,
+          50,
+          MediaQuery.of(context).size.height * .45,
+          50,
+          MediaQuery.of(context).size.height * .10,
         ),
         padding: const EdgeInsets.all(10),
         decoration: BoxDecoration(
-            color: Color.fromRGBO(245, 245, 245, 1),
+            color: Colors.transparent,
             borderRadius: BorderRadius.circular(15.0)),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
-            Center(
-              child: SizedBox(
-                height: MediaQuery.of(context).size.height * .25,
-                child: Image.asset("assets/3.png"),
-              ),
+            Container(
+              width: 51,
+              height: 51,
+              child: Image.asset("assets/page2/login_icon.png"),
             ),
-            Center(
+            SizedBox(height: 30,),
+            Container(
+              alignment: Alignment.topRight,
               child: SizedBox(
-                height: min(30.0,max_item_height),
-                child: Text(
-                  title,
-                  style: const TextStyle(fontWeight: FontWeight.bold),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          title,
+                          style: const TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          detail,
+                          style: const TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                              color: AppColors.lightShadow),
+                        ),
+                      ],
+                    ),
+                    SizedBox(width: 5,),
+                    SizedBox(
+                        width: 5,
+                        height: 74,
+                        child: ColoredBox(color: AppColors.green)),
+                  ],
                 ),
               ),
             ),
-            SizedBox(
-              height: min(30,max_item_height),
-              child: Text(detail),
-            ),
+            SizedBox(height: 10),
             const Spacer(),
             Container(
               padding: const EdgeInsets.only(left: 15, right: 15),
               decoration: BoxDecoration(
                   border: Border.all(),
-                  borderRadius: BorderRadius.circular(20.0)),
-              height: min(50.0,max_item_height),
+                  borderRadius: BorderRadius.circular(27.0)),
+              height: 57,
               child: Row(children: [
-                Icon(Icons.phone),
+                Icon(Icons.phone_rounded),
                 Spacer(),
                 Expanded(
                   flex: 20,
@@ -83,16 +107,17 @@ class LoginPhoneNumber extends StatelessWidget {
               ]),
             ),
             const Spacer(
-              flex: 3,
+              flex: 2,
             ),
             Container(
-              height: min(50.0,max_item_height),
+              height: 54,
               decoration:
-                  BoxDecoration(borderRadius: BorderRadius.circular(20.0)),
+                  BoxDecoration(borderRadius: BorderRadius.circular(27.0)),
               child: ElevatedButton(
                 style: ElevatedButton.styleFrom(
+                  backgroundColor: AppColors.green,
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(0.0),
+                    borderRadius: BorderRadius.circular(27.0),
                   ),
                   padding: EdgeInsets.zero,
                   elevation: 4,
@@ -100,18 +125,14 @@ class LoginPhoneNumber extends StatelessWidget {
                 onPressed: () {
                   BlocProvider.of<LoginCubit>(context).sendSms();
                 },
-                child: Ink.image(
-                  image: const AssetImage('assets/5.png'),
-                  fit: BoxFit.cover,
-                  child: SizedBox(
+                child: SizedBox(
                     width: double.infinity,
-                    height: double.infinity,
+                    height: 54,
                     child: Center(
                       child: Text(
                           style: const TextStyle(color: Colors.white),
                           buttonText),
                     ),
-                  ),
                 ),
               ),
             ),
